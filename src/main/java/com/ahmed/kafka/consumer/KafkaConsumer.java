17093570e7b201dc8af0a6fc4070b3c1f4868062
Ahmed tiba <ahmed.tiba.1993@gmail.com>
2024-07-21 19:42:22 +0100

    Adjust the REST API and consumer student json

--- src/main/java/com/ahmed/kafka/consumer/KafkaConsumer.java
+++ src/main/java/com/ahmed/kafka/consumer/KafkaConsumer.java
@@ -1,5 +1,6 @@
 package com.ahmed.kafka.consumer;
 
+import com.ahmed.kafka.payload.Student;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -12,5 +13,10 @@ public class KafkaConsumer {
     @KafkaListener(topics = "topic1", groupId = "myGroup")
     public void consumeMsg(String topic) {
         log.info("consume msg topic:{}", topic);
+    }
+
+    @KafkaListener(topics = "topic2", groupId = "myGroup")
+    public void consumeStudent(Student student) {
+        log.info("consume student topic:{}", student);
     }
 }
--- src/main/java/com/ahmed/kafka/rest/MessageController.java
+++ src/main/java/com/ahmed/kafka/rest/MessageController.java
@@ -1,5 +1,7 @@
 package com.ahmed.kafka.rest;
 
+import com.ahmed.kafka.payload.Student;
+import com.ahmed.kafka.producer.KafkaJsonProducer;
 import com.ahmed.kafka.producer.KafkaProducer;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -15,9 +17,17 @@ public class MessageController {
 
     private final KafkaProducer kafkaProducer;
 
+    private final KafkaJsonProducer kafkaJsonProducer;
+
     @PostMapping
     public ResponseEntity<String> sendMessage(@RequestBody String message) {
         kafkaProducer.send(message);
         return ResponseEntity.ok(message);
+    }
+
+    @PostMapping("/json")
+    public ResponseEntity<String> sendMessage(@RequestBody Student student) {
+        kafkaJsonProducer.sendStudent(student);
+        return ResponseEntity.ok("Student sent to kafka");
     }
 }
