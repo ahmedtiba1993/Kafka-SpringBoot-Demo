385bf934ba09d733211205575c5bd83b9e0721e7
Ahmed tiba <ahmed.tiba.1993@gmail.com>
2024-07-21 16:27:23 +0100

    first commit

---
+++ src/test/java/com/ahmed/kafka/KafkaSpringBootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ahmed.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaSpringBootDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
