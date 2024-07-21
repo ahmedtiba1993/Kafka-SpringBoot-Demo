b8e8db2c5272d4baebe41e798b7a979d986a3748
Ahmed tiba <ahmed.tiba.1993@gmail.com>
2024-07-21 18:23:00 +0100

    Create a Kafka Producer

---
+++ src/main/java/com/ahmed/kafka/producer/KafkaProducer.java
@@ -0,0 +1,20 @@
+package com.ahmed.kafka.producer;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class KafkaProducer {
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public void send(String message) {
+        log.info("send message to topic1: {}", message);
+        kafkaTemplate.send("topic1", message);
+    }
+
+}
